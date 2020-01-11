94da0970e134cc6ed0c73cb079c7aabf0b27cf7d
Cameron Showalter <cjshowalter@alaska.edu>
2020-01-10 18:27:52 -0900

    Added main method of pytest. Added code that sends test to right method, based on pytest_conf.yml

--- test_MainManager.py
+++ test_MainManager.py
@@ -1,23 +1,28 @@
-import yaml
 import conftest as helpers
+import sys, os, pytest
 
 # Lets you import files in the directory behind this one:
-import sys, os
 project_root = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
 sys.path.insert(1, project_root)
-from tests import pytest_managers as tests
 
+main_config = helpers.loadConfigFromDirectory(os.path.join(project_root, "yml_tests"))
+all_tests = helpers.loadTestsFromDirectory(project_root, recurse=True)
 
+# For each item in the list, run it though the suite:
+@pytest.mark.parametrize("test", all_tests)
+def test_main(test, cli_args):
+	test_info = test[0]	# That test's info
+	file_conf = test[1] # Any extra info in that test's yml
+	# Skip the test if needed:
+	helpers.skipTestsIfNecessary(test_info["title"], file_conf["yml name"], cli_args)
 
-file_config = os.path.join(project_root, "tests", "pytest_config.yml")
-
-try:
-	file_config = open(file_config, "r")
-	file_config = yaml.safe_load(file_config)
-except (OSError, IOError):
-    assert False, "Error opening yaml {0}. Does it exist?".format(project_root)
-except (yaml.YAMLError, yaml.MarkedYAMLError) as e:
-    assert False, "Error parsing yaml {0}. Error: {1}.".format(project_root, str(e))
-
-test_list = helpers.loadTestsFromDirectory(project_root, recurse=True)
-print(test_list)
+	# pass the values to the right function:
+	found_test = False
+	for conf in main_config["test_types"]:
+		# If all of the required keys are in the test, you found it:
+		if set(conf["required_keys"]).issubset(test_info):
+			found_test = True
+			# Run the test:
+			conf["method"](test_info, file_conf)
+			break
+	assert found_test, "Cannot determine test type. Test: {0}. File: {1}.".format(test_info["title"], file_conf["yml name"])
