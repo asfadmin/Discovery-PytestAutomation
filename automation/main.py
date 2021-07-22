c89d9fd54968f1b6e3403febb5a60e29e64db53d
Cameron Showalter <cjshowalter@alaska.edu>
2021-07-21 17:24:26 -0800

    Basic start for running yml tests in plugin. (Ripping out parametrize completely)

--- automation/main.py
+++ automation/main.py
@@ -1,66 +1,103 @@
 from . import auto as helpers
 import os
 import pytest
+import yaml
 from  copy import deepcopy
 
-# project root = One dir back from the dir this file is in
-project_root = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
-main_config = helpers.getConfig()
-all_tests = helpers.loadTestsFromDirectory(project_root, recurse=True)
+def pytest_sessionstart(session):
+    # 1) Find pytest_manager.py and pytest_config.yml
+    # 2) Load those methods, import what's needed. Save as global (to load in each YamlItem)
+    # 3) If hook is defined in pytest_config, run it here.
+    pass
 
-# For each item in the list, run it though the suite:
-@pytest.mark.parametrize("test", all_tests)
-def test_main(test, cli_args):
-    test_info = test[0] # That test's info
-    file_conf = test[1] # Any extra info in that test's yml
-    # Basic error reporting, for when a test fails:
-    error_msg = "\nTitle: '{0}'".format(test_info["title"]) + "\nFile: '{0}'\n".format(file_conf["yml name"])
+# Based on: https://docs.pytest.org/en/6.2.x/example/nonpython.html
+def pytest_collect_file(parent, path):
+    if path.ext in [".yml", ".yaml"] and path.basename.startswith("test_"):
+        return YamlFile.from_parent(parent, fspath=path, test="asdf")
 
-    # Skip the test if needed:
-    helpers.skipTestsIfNecessary(test_info["title"], file_conf["yml name"], cli_args)
+class YamlFile(pytest.File):
+    def __init__(self, parent, fspath, test):
+        super().__init__(parent=parent, fspath=fspath)
+        self.test = test
 
-    # pass the values to the right function:
-    found_test = False
-    for conf in main_config["test_types"]:
+    def collect(self):
+        data = yaml.safe_load(self.fspath.open())
+        if "tests" in data:
+            for test in data["tests"]:
+                # Get the individual key/val
+                name, info = next(iter( test.items() ))
+                yield YamlItem.from_parent(self, name=name, item=info)
+
+class YamlItem(pytest.Item):
+    def __init__(self, name, parent, item):
+        super().__init__(name, parent)
+        self.item = item
+    
+    def runtest(self):
+        pass
+
+
+
+
+
+# # project root = One dir back from the dir this file is in
+# project_root = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
+# main_config = helpers.getConfig()
+# all_tests = helpers.loadTestsFromDirectory(project_root, recurse=True)
+
+# # For each item in the list, run it though the suite:
+# @pytest.mark.parametrize("test", all_tests)
+# def test_main(test, cli_args):
+#     test_info = test[0] # That test's info
+#     file_conf = test[1] # Any extra info in that test's yml
+#     # Basic error reporting, for when a test fails:
+#     error_msg = "\nTitle: '{0}'".format(test_info["title"]) + "\nFile: '{0}'\n".format(file_conf["yml name"])
+
+#     # Skip the test if needed:
+#     helpers.skipTestsIfNecessary(test_info["title"], file_conf["yml name"], cli_args)
+
+#     # pass the values to the right function:
+#     found_test = False
+#     for conf in main_config["test_types"]:
         
-        ## BEGIN normal test_type parsing:
+#         ## BEGIN normal test_type parsing:
 
-        # If you declare it, make sure the keys are within that test.
-        if "required_keys" not in conf or set(conf["required_keys"]).issubset(test_info):
-            passed_key_check = True
-        else:
-            passed_key_check = False
+#         # If you declare it, make sure the keys are within that test.
+#         if "required_keys" not in conf or set(conf["required_keys"]).issubset(test_info):
+#             passed_key_check = True
+#         else:
+#             passed_key_check = False
 
-        # If all the tests in a file, belong to a test type:
-        if "required_files" not in conf or file_conf["yml name"] in conf["required_files"]:
-            passed_file_check = True
-        else:
-            passed_file_check = False
+#         # If all the tests in a file, belong to a test type:
+#         if "required_files" not in conf or file_conf["yml name"] in conf["required_files"]:
+#             passed_file_check = True
+#         else:
+#             passed_file_check = False
 
-        # Run the test, if all the checks agree:
-        # (I broke this out seperatly, to add more later easily, and to allow you to use more than one at once)
-        if passed_key_check and passed_file_check:
-            found_test = True
-            ## Check if the tester want's to run/exclude a specific *type* of test:
-            # (Can't do this any sooner, needs to make sure the for-loop is on your test-type)
-            if cli_args["only run type"] != None:
-                title_hit_in_list = False
-                for only_run_each in cli_args["only run type"]:
-                    if only_run_each.lower() in conf["title"].lower():
-                        title_hit_in_list = True
-                        break
-                if not title_hit_in_list:
-                    pytest.skip("Type of test did not contain --only-run-type param (case insensitive)")
-            # Same, but reversed for 'dont run':
-            if cli_args["dont run type"] != None:
-                for dont_run_each in cli_args["dont run type"]:
-                    if dont_run_each.lower() in conf["title"].lower():
-                        pytest.skip("Type of test contained --dont-run-type param (case insensitive)")
+#         # Run the test, if all the checks agree:
+#         # (I broke this out seperatly, to add more later easily, and to allow you to use more than one at once)
+#         if passed_key_check and passed_file_check:
+#             found_test = True
+#             ## Check if the tester want's to run/exclude a specific *type* of test:
+#             # (Can't do this any sooner, needs to make sure the for-loop is on your test-type)
+#             if cli_args["only run type"] != None:
+#                 title_hit_in_list = False
+#                 for only_run_each in cli_args["only run type"]:
+#                     if only_run_each.lower() in conf["title"].lower():
+#                         title_hit_in_list = True
+#                         break
+#                 if not title_hit_in_list:
+#                     pytest.skip("Type of test did not contain --only-run-type param (case insensitive)")
+#             # Same, but reversed for 'dont run':
+#             if cli_args["dont run type"] != None:
+#                 for dont_run_each in cli_args["dont run type"]:
+#                     if dont_run_each.lower() in conf["title"].lower():
+#                         pytest.skip("Type of test contained --dont-run-type param (case insensitive)")
             
-            ## Run the test:
-            conf["method_pointer"](test_info, deepcopy(file_conf), deepcopy(cli_args), deepcopy(conf["variables"]))
-            break
-    assert found_test, "Could not find what test this block belongs to. {0}".format(error_msg)
+#             ## Run the test:
+#             conf["method_pointer"](test_info, deepcopy(file_conf), deepcopy(cli_args), deepcopy(conf["variables"]))
+#             break
+#     assert found_test, "Could not find what test this block belongs to. {0}".format(error_msg)
 
 ## Custom CLI options: 
 def pytest_addoption(parser):
