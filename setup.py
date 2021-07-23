ee6211542abe54a7ad384877e2fe58c36c67123a
Cameron Showalter <cjshowalter@alaska.edu>
2021-07-23 13:56:56 -0800

    Updating to reflect package restructure

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ with open("README.md", "r") as f:
 
 setup(
     name="pytest-automation",
-    description="pytest plugin for building a test suite, using YAML files.",
+    description="pytest plugin for building a test suite, using YAML files to extend pytest parameterize functionality.",
     long_discription=long_discription,
     long_description_content_type="text/markdown",
     packages=["pytestautomation"],
     url="https://github.com/asfadmin/Discovery-PytestAutomation",
     # the following makes a plugin available to pytest
-    entry_points={"pytest11": ["pytestautomation = automation.plugin"]},
+    entry_points={"pytest11": ["pytestautomation = pytestautomation.plugin"]},
     # custom PyPI classifier for pytest plugins
     classifiers=["Framework :: Pytest"],
     install_requires=[
