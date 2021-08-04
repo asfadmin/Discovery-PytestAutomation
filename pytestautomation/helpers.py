0c7a9f6612e92f56e0d2cd9043d985a890c41026
Cameron Showalter <cjshowalter@alaska.edu>
2021-08-04 15:57:09 -0800

    Runs out of pytest root, intead of current dir. Added check to not override 'title' if already inside yml test: error instead. renamed cli_config to config, for consistancy

--- pytestautomation/helpers.py
+++ pytestautomation/helpers.py
@@ -8,8 +8,8 @@ import pytest       # skip
 import warnings     # warn
 
 
-def removeSubmodulePaths(paths: list) -> list:
-    submodule_path = os.path.join(os.getcwd(), ".gitmodules")
+def removeSubmodulePaths(paths: list, rootdir: str) -> list:
+    submodule_path = os.path.join(rootdir, ".gitmodules")
     # If there are no submodules, nothing to do
     if not os.path.isfile(submodule_path):
         return paths
@@ -23,12 +23,12 @@ def removeSubmodulePaths(paths: list) -> list:
             valid_paths.append(path)
     return valid_paths        
 
-def getFileFromName(name: str) -> str:
+def getFileFromName(name: str, rootdir: str) -> str:
     # From your current dir, find all the files with this name:
-    recursive_path = os.path.abspath(os.path.join(os.getcwd(), "**", name))
+    recursive_path = os.path.abspath(os.path.join(rootdir, "**", name))
     possible_paths = glob.glob(recursive_path, recursive=True)
     # If any are in a sub-repo/submodule, ignore it. (They might have their *own* config):
-    possible_paths = removeSubmodulePaths(possible_paths)
+    possible_paths = removeSubmodulePaths(possible_paths, rootdir)
     # Make sure you got only found one config:
     assert len(possible_paths) == 1, "WRONG NUMBER OF CONFIGS: Must have exactly one '{0}' file inside project. Found {1} instead.\nBase path used to find files: {2}.".format(name, len(possible_paths), recursive_path)
     return possible_paths[0]
@@ -65,9 +65,11 @@ def loadYamlFile(path: str, required: bool=False):
 #    file to report error if something's not formated
 def seperateKeyVal(mydict: dict, file: str):
     num_test_titles = len(list(mydict.keys()))
-    assert num_test_titles == 1, "MISFORMATTED TEST: {0} keys found in a test. Only have 1, the title of the test. File: {1}".format(num_test_titles, file)
+    assert num_test_titles == 1, "MISFORMATTED TEST: {0} keys found in a test. Only have 1, the title of the test. File: '{1}'.".format(num_test_titles, file)
     # return the individual key/val
     title, test_info = next(iter( mydict.items() ))
+    # Make sure the title key isn't in use already, it's reserved:
+    assert "title" not in test_info, "MISFORMATTED TEST: 'title' key found in test '{0}'. This key is reserved for internal use only. File '{1}'.".format(title, file)
     # Save title to test_info. (Might seem reduntant, but this gets all test_info keys at base level, AND still saves the test title)
     test_info["title"] = title
     return test_info
@@ -84,17 +86,17 @@ def getPytestManagerModule(pytest_managers_path: str):
     sys.path.remove(os.path.dirname(pytest_managers_path))
     return pytest_managers_module
 
-def skipTestsIfNecessary(cli_config, test_name, file_name, test_type):
+def skipTestsIfNecessary(config, test_name, file_name, test_type):
     # If they want to skip EVERYTHING:
-    if cli_config.getoption("--skip-all"):
+    if config.getoption("--skip-all"):
         pytest.skip("Skipping ALL tests. (--skip-all cli arg was found).")
     
     ### ONLY/DONT RUN NAME:
     # If they only want to run something, based on the test title:
-    if cli_config.getoption("--only-run-name") is not None:
+    if config.getoption("--only-run-name") is not None:
         # If you match with ANY of the values passed to "--only-run-name":
         found_in_title = False
-        for only_run_filter in cli_config.getoption("--only-run-name"):
+        for only_run_filter in config.getoption("--only-run-name"):
             if only_run_filter.lower() in test_name.lower():
                 # Found it!
                 found_in_title = True
@@ -103,18 +105,18 @@ def skipTestsIfNecessary(cli_config, test_name, file_name, test_type):
         if not found_in_title:
             pytest.skip("Title of test did not contain --only-run-name param (case insensitive)")
     # If they DONT want to run something, based on test title:
-    if cli_config.getoption("--dont-run-name") is not None:
+    if config.getoption("--dont-run-name") is not None:
         # Nice thing here is, you can skip the second you find it:
-        for dont_run_filter in cli_config.getoption("--dont-run-name"):
+        for dont_run_filter in config.getoption("--dont-run-name"):
             if dont_run_filter.lower() in test_name.lower():
                 pytest.skip("Title of test contained --dont-run-name param (case insensitive)")
     
     ### ONLY/DONT RUN FILE:
     # If they only want to run something, based on the file name:
-    if cli_config.getoption("--only-run-file") is not None:
+    if config.getoption("--only-run-file") is not None:
         # If you match with ANY of the values passed to "--only-run-file":
         found_in_file = False
-        for only_run_filter in cli_config.getoption("--only-run-file"):
+        for only_run_filter in config.getoption("--only-run-file"):
             if only_run_filter.lower() in file_name.lower():
                 # Found it!
                 found_in_file = True
@@ -123,18 +125,18 @@ def skipTestsIfNecessary(cli_config, test_name, file_name, test_type):
         if not found_in_file:
             pytest.skip("Name of file did not contain --only-run-file param (case insensitive)")
     # If they DONT want to run something, based on file name:
-    if cli_config.getoption("--dont-run-file") is not None:
+    if config.getoption("--dont-run-file") is not None:
         # Nice thing here is, you can skip the second you find it:
-        for dont_run_filter in cli_config.getoption("--dont-run-file"):
+        for dont_run_filter in config.getoption("--dont-run-file"):
             if dont_run_filter.lower() in file_name.lower():
                 pytest.skip("Name of file contained --dont-run-file param (case insensitive)")
 
     ### ONLY/DONT RUN TYPE:
     # If they only want to run something, based on the test type:
-    if cli_config.getoption("--only-run-type") is not None:
+    if config.getoption("--only-run-type") is not None:
         # If you match with ANY of the values passed to "--only-run-type":
         found_in_type = False
-        for only_run_filter in cli_config.getoption("--only-run-type"):
+        for only_run_filter in config.getoption("--only-run-type"):
             if only_run_filter.lower() in test_type.lower():
                 # Found it!
                 found_in_type = True
@@ -143,9 +145,9 @@ def skipTestsIfNecessary(cli_config, test_name, file_name, test_type):
         if not found_in_type:
             pytest.skip("Test type did not did not contain --only-run-type param (case insensitive)")
     # If they DONT want to run something, based on test type:
-    if cli_config.getoption("--dont-run-type") is not None:
+    if config.getoption("--dont-run-type") is not None:
         # Nice thing here is, you can skip the second you find it:
-        for dont_run_filter in cli_config.getoption("--dont-run-type"):
+        for dont_run_filter in config.getoption("--dont-run-type"):
             if dont_run_filter.lower() in test_type.lower():
                 pytest.skip("Test type contained --dont-run-file param (case insensitive)")
 
