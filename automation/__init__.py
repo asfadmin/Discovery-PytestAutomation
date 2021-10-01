358c83dbf4d2c3a354ea157412092cc8d14aaf86
Cameron Showalter <cjshowalter@alaska.edu>
2021-10-01 10:41:21 -0800

    Now declaring __version__ for package

--- automation/__init__.py
+++ automation/__init__.py
@@ -1,12 +1,9 @@
 from packaging.version import Version, InvalidVersion
-import subprocess
-
-# Get the version from git and save it:
-package_version = subprocess.run(['git', 'describe', '--tags'], stdout=subprocess.PIPE).stdout.decode("utf-8").strip()
+from setuptools_scm import get_version
 
 try:
     # This guarantees it to be PEP 440 compliant. (Which sometimes, git creates a non-compliant version)
-    __version__ = str(Version(package_version))
+    __version__ = str(Version(get_version()))
 except InvalidVersion:
     __version__ = "0.0.0"
 
