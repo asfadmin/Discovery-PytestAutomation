2fb13a6f44a628dca4e10c8111581ad414b9d3fb
Cameron Showalter <cjshowalter@alaska.edu>
2021-10-01 10:41:04 -0800

    Moving build requirements to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,26 +1,44 @@
 import setuptools
-from automation import __version__
-
+# from automation import __version__
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
 setuptools.setup(
     name="pytest-automation",
-    version=__version__,
-    # use_scm_version=True,
+    # version=__version__,
+    use_scm_version=True,
     description="pytest plugin for building a test suite, using YAML files to extend pytest parameterize functionality.",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    author="Alaska Satellite Facility Discovery Team",
+    author_email="uaf-asf-discovery@alaska.edu",
     packages=["automation"],
     url="https://github.com/asfadmin/Discovery-PytestAutomation",
     # the following makes a plugin available to pytest
     entry_points={"pytest11": ["pytest-automation = automation.plugin"]},
     # custom PyPI classifier for pytest plugins
-    classifiers=["Framework :: Pytest"],
+    classifiers=[
+        "Framework :: Pytest",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Software Development :: Testing"
+    ],
     install_requires=[
         'pytest',
         'PyYAML',
-        'packaging'
-    ]
+    ],
+    license='BSD',
+    license_files=('LICENSE',),
+
+
+    ## After creating test suite, figure out if these keys are useful:
+    #test_suite='???',
+    #tests_require=['???'],
 )
